caed95be1e40983ea3c9190aaccfcd55a27da8d0
Victor Jolissaint <victor.jolissaint@gmail.com>
2015-08-12 23:49:21 +0100

    Add day and time column and basic plot function

--- basisData.R
+++ basisData.R
@@ -1,9 +1,34 @@
-basisData <- function(csvFile) {
+library(ggplot2)
+library(dplyr)
+
+readAndFormatData <- function(csvFile) {
   data <- read.csv(csvFile, na.strings = "")
   
   ## Convert date column as Date type
-  data[["date"]] <- as.Date(data[["date"]])
+  #data <- data %>% mutate(fulldate = as.POSIXct(date, "%Y-%m-%d %H:%M", tz="UTC"))
+  #data[["date"]] <- as.Date(data[["date"]])
+  
+  ## Add a day and time column
+  ## XXX don't know why it doesn't work in one line :(
+  data <- data %>% mutate(time = strftime(date, format="%H:%M", tz="UTC"))
+  data <- data %>% mutate(day = strftime(date, format="%Y-%m-%d", tz="UTC"))
+  
+  return(data)
+}
 
+basicPlot <- function(csvFile) {
+  d <- readAndFormatData(csvFile)
+  
+  # Filter minute I didn't move (steps = 0) and a weird value where steps = 12000, WTF?!
+  d <- d %>% filter(steps > 0) %>% filter(steps < 1000)
+  
+  #First basic plot of the last 10000 non-null mesures
+  qplot(data=tail(d,10000),x=time,y=steps,main="Steps per minute", color=day)
+}
+
+basicData <- function(csvFile) {
+  data <- readAndFormatData(csvFile)
+  
   nbMinutes <- nrow(data)
   #print(nbMinutes)
   
