a138f5598308e2e827bdff78cebe50a988ed4c74
Victor Jolissaint <victor.jolissaint@gmail.com>
2015-08-14 00:43:15 +0100

    Add axis titles and set nicer scale on x

--- basisData.R
+++ basisData.R
@@ -16,46 +16,78 @@ readAndFormatData <- function(csvFile) {
   return(data)
 }
 
-basicPlot <- function(csvFile) {
-  d <- readAndFormatData(csvFile)
+basicPlot <- function(d) {
+  ## Filter a weird value where steps = 12000, WTF?!
+  d <- d %>% filter(steps < 1000)
   
-  # Filter minute I didn't move (steps = 0) and a weird value where steps = 12000, WTF?!
-  d <- d %>% filter(steps > 0) %>% filter(steps < 1000)
+  x_scale <- genHourScale()
   
-  #First basic plot of the last 10000 non-null mesures
-  qplot(data=tail(d,10000),x=time,y=steps,main="Steps per minute", color=day)
+  ## First basic plot
+  ## x = 24hours minutes by minutes
+  ## y = steps per minute for a given minute of the day
+  qplot(data=d, x=time, y=steps, main="Steps per minute over 2 years", color=day) + 
+    labs(x="Time 00:00 to 24:00", y="Steps / min", title="Steps per minute over days") + 
+    scale_x_discrete(breaks=x_scale)
 }
 
-basicData <- function(csvFile) {
-  data <- readAndFormatData(csvFile)
+histogram <- function(d) {
+  ## Filter a weird value where steps = 12000, WTF?!
+  d <- d %>% filter(steps < 1000)
+
+  x_scale <- genHourScale()
   
-  nbMinutes <- nrow(data)
+  ggplot(data=d, aes(x=time, y=steps)) + 
+    geom_bar(stat="identity", fill="black", alpha=1/10) + 
+    labs(x="Time 00:00 to 24:00", y="Steps / min", title="Steps per minute over days") + 
+    scale_x_discrete(breaks=x_scale) 
+}
+
+genHourScale <- function() {
+  c <- seq(
+    from=as.POSIXct("00:00", format="%H:%M", tz="UTC"),
+    by="2 hour",
+    length.out = 12
+  )
+  strftime(c, format="%H:%M")
+}
+
+basicData <- function(d) {
+
+  nbMinutes <- nrow(d)
   #print(nbMinutes)
   
-  statsByColName <- function(data, col) {
+  statsByColName <- function(d, col) {
     ## filter NA value
-    output <- data[!is.na(data[,col]), col]
+    output <- d[!is.na(d[,col]), col]
     summary(output)
   }
   
-  sumByColName <- function(data, col) {
+  sumByColName <- function(d, col) {
     ## filter NA value
-    output <- data[!is.na(data[,col]), col]
+    output <- d[!is.na(d[,col]), col]
     sum(output)
   }
   
   ## Heart Rate
-  hr <- statsByColName(data, "heart.rate")
+  hr <- statsByColName(d, "heart.rate")
   print("Heart rate:")
   print(hr)
   
   ## Sum Steps
-  steps <- sumByColName(data, "steps")
+  steps <- sumByColName(d, "steps")
   print("Sum steps:")
   print(steps)
   
   ## Skin Temp (F)
-  temp <- statsByColName(data, "skin.temp")
+  temp <- statsByColName(d, "skin.temp")
   print("Skin temp:")
   print(temp)
+}
+
+basisData <- function(csvFile){
+  d <- readAndFormatData(csvFile)
+  
+  #basicData(d)
+  basicPlot(d)
+  #histogram(d) ## XXX working on it
 }
